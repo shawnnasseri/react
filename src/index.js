89615f7c6ffa155d2ade59c84ab81376b1e2aedc
Shawn Nasseri <48268989+shawnnasseri@users.noreply.github.com>
2024-04-28 19:14:15 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import GuessApp from './GuessApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GuessApp />
   </React.StrictMode>
 );
 
